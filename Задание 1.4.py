f859145f952f7320a15e7eb4fedefeb883b2cf44
Матвей <66694005+Matvyxa@users.noreply.github.com>
2020-06-14 22:55:53 +0300

    Задание 1.4
    Не правильно искало больше значение, исправил

--- Задание 1.4.py
+++ Задание 1.4.py
@@ -1,7 +1,13 @@
-a = int(input('Введите целое число '))
-b = a % 10
-while b >= a:
-    b = b / 10
-print(b)
+# не смог свое первоначальное решение адаптировать, скопировал решение через хранилище
+num_init = int(input("Ввведите целое положительное число: "))
+zero = 0
+num = num_init
+while num > 0:
+    big = num % 10
+    if big > zero:
+        zero = big
+    num = num // 10
+print(f"Наибольшая цифра в числе {num_init} равна {zero}")
+
 
 
