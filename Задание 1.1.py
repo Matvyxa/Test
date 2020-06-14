6828eb557540edf7a23dc98b2f462dc3fa500b7e
Матвей <66694005+Matvyxa@users.noreply.github.com>
2020-06-14 22:49:44 +0300

    Задание 1.1
    Подправил форматирование

--- Задание 1.1.py
+++ Задание 1.1.py
@@ -1,8 +1,8 @@
-a = input('введите Ваше имя')
-print(f"{a}")
-b = input('введите Вашу фамилию')
-print(f"{ b}")
-c = b + a
-print(f"Добрый вечер, {c}")
-d = int(input('Укажите Ваш возраст'))
-print(f"Ваш возраст, {d}")
+name = input("введите Ваше имя")
+print(f"{name}")
+surmane = input("введите Вашу фамилию")
+print(f"{surmane}")
+human = surmane + name
+print(f"Добрый вечер, {human}")
+age = int(input("Укажите Ваш возраст"))
+print(f"Ваш возраст, {age}")
